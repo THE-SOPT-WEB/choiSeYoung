30a92aabcb2033e15a09e8db444be116fe89a0da
say-young516 <sychoi0516@naver.com>
2022-04-22 16:59:38 +0900

    feat: 정답/오답 처리, 모달창 띄우고 닫기

--- main.js
+++ main.js
@@ -37,8 +37,45 @@ function initGame({score,answer,image}){
 
   image.src=quizList[currentStep].src;
 }
+
+function showModal(modalContent){
+  const modal=$('.modal');
+  const modalBody = $('p.modal__body');
+  modalBody.innerText = modalContent;
+  
+  modal.classList.remove('hide');
+  //1.5초 뒤에 모달 클래스에 hide 추가! --> 이렇게 하면 모달 자동적으로 없어짐
+  setTimeout(()=>{
+    modal.classList.add('hide');
+  },1500);
+}
+function attachEvent({score,answer, image}){
+  answer.addEventListener('click',(e)=>{
+    
+    //뒤에 있는 요소에 속하는가?
+    if(e.target instanceof HTMLElement){
+      const currentAnswer = e.target.innerText;
+      const realAnswer = quizList[currentStep].answer;
+      if(currentAnswer ===realAnswer){
+        //정답인 경우 처리~~
+        showModal('나를 알아주다니 고마웡');
+      }
+      else{
+        //오답인 경우~~
+
+        showModal('아니얌')
+      }
+    }
+  });
+}
+
+function goNextStep(){
+  // 점수 올리기, 이미지 바꿔주기
+  
+}
 function gameManager(gameInfo){
   initGame(gameInfo);
+  attachEvent(gameInfo);
 }
 window.onload = () => {
   gameManager(
