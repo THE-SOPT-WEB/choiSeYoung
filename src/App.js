6b15d46055ffd5c6a48473dcd10da7d5e7db6b3a
say-young516 <sychoi0516@naver.com>
2022-05-03 15:34:44 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const gameInfo = [
+    {
+      img: "../img/민트 초콜릿 칩.png",
+      name: "민트 초콜릿 칩",
+    },
+    {
+      img: "../img/베리베리 스트로베리.png",
+      name: "베리베리 스트로베리",
+    },
+    {
+      img: "../img/엄마는 외계인.png",
+      name: "엄마는 외계인",
+    },
+    {
+      img: "../img/초코나무 숲.png",
+      name: "베리베리 스트로베리",
+    },
+  ];
+  return <></>;
+}
+
+export default App;
